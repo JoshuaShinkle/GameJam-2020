3d2e7a8d801115376dcdc4dbe6f026dda6f38aeb
Joshua Shinkle <joshuashinkle@beta.campus.tayloru.edu>
2020-10-23 21:03:43 -0400

    Player can move using arrow keys

--- survive.py
+++ survive.py
@@ -2,7 +2,7 @@ import sys, pygame
 pygame.init()
 
 size = width, height = 800,600
-speed = [2, 2]
+speed = 5
 white = 255, 255, 255
 
 screen = pygame.display.set_mode(size)
@@ -17,6 +17,16 @@ lance_rect.centery = height/2
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
+
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            lance_rect = lance_rect.move(-speed,0)
+        if event.key == pygame.K_RIGHT:
+            lance_rect = lance_rect.move(speed,0)
+        if event.key == pygame.K_UP:
+            lance_rect = lance_rect.move(0,-speed)
+        if event.key == pygame.K_DOWN:
+            lance_rect = lance_rect.move(0,speed)
 
     screen.fill(white)
     screen.blit(lance, lance_rect)
