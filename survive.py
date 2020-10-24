463fdb022039c1dfa616ef2d44668ad85c3b2b1a
Joshua Shinkle <joshuashinkle@beta.campus.tayloru.edu>
2020-10-23 22:10:34 -0400

    Enemy class created

--- survive.py
+++ survive.py
@@ -1,4 +1,4 @@
-import sys, pygame
+import sys, pygame, random
 pygame.init()
 
 size = width, height = 800,600
@@ -13,11 +13,19 @@ lance_rect = lance.get_rect()
 lance_rect.centerx = width/2
 lance_rect.centery = height/2
 
-enemy = "tile036.png"
-enemy = pygame.image.load(enemy)
-enemy_rect = enemy.get_rect()
-enemy_rect.centerx = width/3
-enemy_rect.centery = height/3
+class Enemy:
+    def __init__(self):
+        self.imageName = "tile036.png"
+        self.loadedImage = pygame.image.load(self.imageName)
+        self.image_rect = self.loadedImage.get_rect()
+
+enemy1 = Enemy()
+enemy1.image_rect.centerx = width/3
+enemy1.image_rect.centery = height/3
+
+enemy2 = Enemy()
+enemy2.image_rect.centerx = width/4
+enemy2.image_rect.centery = height/4
 
 while 1:
     for event in pygame.event.get():
@@ -38,5 +46,6 @@ while 1:
 
     screen.fill(white)
     screen.blit(lance, lance_rect)
-    screen.blit(enemy, enemy_rect)
+    screen.blit(enemy1.loadedImage, enemy1.image_rect)
+    screen.blit(enemy2.loadedImage, enemy2.image_rect)
     pygame.display.flip()
