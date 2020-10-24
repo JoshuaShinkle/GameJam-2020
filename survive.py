afaa6967a904b7a8a6875efd63d40e1cf8a0d0d6
Joshua Shinkle <joshuashinkle@beta.campus.tayloru.edu>
2020-10-23 22:37:25 -0400

    Enemy randomly spawns off screen

--- survive.py
+++ survive.py
@@ -1,31 +1,55 @@
 import sys, pygame, random
 pygame.init()
 
+# Screen Setup
 size = width, height = 800,600
 speed = 5
 white = 255, 255, 255
-
 screen = pygame.display.set_mode(size)
 
-lance = "tile024.png"
-lance = pygame.image.load(lance)
-lance_rect = lance.get_rect()
-lance_rect.centerx = width/2
-lance_rect.centery = height/2
-
+# Character Setup
 class Enemy:
     def __init__(self):
         self.imageName = "tile036.png"
         self.loadedImage = pygame.image.load(self.imageName)
         self.image_rect = self.loadedImage.get_rect()
-
 enemy1 = Enemy()
-enemy1.image_rect.centerx = width/3
-enemy1.image_rect.centery = height/3
 
-enemy2 = Enemy()
-enemy2.image_rect.centerx = width/4
-enemy2.image_rect.centery = height/4
+lance = "tile024.png"
+lance = pygame.image.load(lance)
+lance_rect = lance.get_rect()
+
+# Character Spawn
+lance_rect.centerx = width/2
+lance_rect.centery = height/2
+
+spawnSide = random.randint(0,3)
+OFFSCREENDISTANCE = 30
+
+if spawnSide == 0:
+    enemySpawnx = -OFFSCREENDISTANCE
+    enemySpawny = random.randint(0, height)
+    enemy1.image_rect.centerx = enemySpawnx
+    enemy1.image_rect.centery = enemySpawny
+elif spawnSide == 1:
+    enemySpawnx = random.randint(0, width)
+    enemySpawny = -OFFSCREENDISTANCE
+    enemy1.image_rect.centerx = enemySpawnx
+    enemy1.image_rect.centery = enemySpawny
+elif spawnSide == 2:
+    enemySpawnx = width + OFFSCREENDISTANCE
+    enemySpawny = random.randint(0, height)
+    enemy1.image_rect.centerx = enemySpawnx
+    enemy1.image_rect.centery = enemySpawny
+elif spawnSide == 3:
+    enemySpawnx = random.randint(0, width)
+    enemySpawny = height + OFFSCREENDISTANCE
+    enemy1.image_rect.centerx = enemySpawnx
+    enemy1.image_rect.centery = enemySpawny
+
+print(spawnSide)
+print(str(enemy1.image_rect.centerx) + ", " + str(enemy1.image_rect.centery))
+print()
 
 while 1:
     for event in pygame.event.get():
@@ -47,5 +71,5 @@ while 1:
     screen.fill(white)
     screen.blit(lance, lance_rect)
     screen.blit(enemy1.loadedImage, enemy1.image_rect)
-    screen.blit(enemy2.loadedImage, enemy2.image_rect)
+    #screen.blit(enemy2.loadedImage, enemy2.image_rect)
     pygame.display.flip()
